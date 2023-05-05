fca9c62fc3a77024bc75d3237f23fab4a9424e52
Teledi_Makhafola <77184819+teledi-makhafola@users.noreply.github.com>
2023-05-05 11:48:13 +0200

    Add files via upload

---
+++ create tablescript.sql
@@ -0,0 +1,15 @@
+create table employees(
+id int primary key,
+name varchar not null ,
+last_Name varchar not null,
+email varchar not null,
+department varchar not null,
+contact_No varchar not null
+);
+
+
+drop table employees s
+
+alter table employees 
+add column gender varchar;
+
